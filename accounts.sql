b70674e2c22f9e5a8e815ab73dd35c3fbf6a01ba
SamP10 <sam.plant998@gmail.com>
2021-03-02 13:54:06 +0000

    working

---
+++ accounts.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 02, 2021 at 02:42 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `accounts`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `group_name` varchar(255) NOT NULL,
+  `roomnumber` int(10) NOT NULL,
+  `numberext` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `group_name`, `roomnumber`, `numberext`) VALUES
+(1, 'admin1', 'admin1', 1, 1234),
+(2, 'staff1', 'Staff', 2, 1235),
+(3, 'staff2', 'Staff', 3, 1236),
+(4, 'dev1', 'Developer', 3, 1237),
+(5, 'dev2', 'Developer', 4, 1238);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ lookup.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+//Setting up database access credentials
+$host='localhost';
+$user='admin';
+$password='tiaspbiqe2r';
+$dbname='accounts';
+//Database Connection with exit message upon error
+$connect = mysqli_connect($host, $user, $password, $dbname) or exit("Unable to connect to database!");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<header>
+
+</header>
+<body>
+<h1>Employee Lookup</h1>
+<form method="POST" action="">
+    Room Number:<input type="number" name="roomnumber"><br>
+    Phone Extension:<input type="number" name="numberext"><br>
+    <button name="submit" type="submit">SEARCH</button>
+</form>
+<?php
+    if(isset($_POST['submit'])){
+        if(isset($_POST['roomnumber'])&&!empty($_POST['roomnumber'])){
+            $roomnumber=$_POST['roomnumber'];
+            $query="SELECT * FROM users WHERE roomnumber=$roomnumber";
+            $run=mysqli_query($connect, $query);
+            while($row=mysqli_fetch_assoc($run)){
+                echo'Username: '.$row['username'];
+                echo'Group: '.$row['group_name'];
+                echo'Room Number: '.$row['roomnumber'];
+                echo'Number Extension: '.$row['numberext'];
+            }
+        }elseif(isset($_POST['numberext'])&&!empty($_POST['numberext'])){
+            $numberext=$_POST['numberext'];
+            $query="SELECT * FROM users WHERE numberext=$numberext";
+            $run=mysqli_query($connect, $query);
+            while($row=mysqli_fetch_assoc($run)){
+                echo'Username: '.$row['username'];
+                echo'Group: '.$row['group_name'];
+                echo'Room Number: '.$row['roomnumber'];
+                echo'Number Extension: '.$row['numberext'];
+            }
+
+        }
+    }
+?>
+</body>
+<footer>
+
+</footer>
+</html>
